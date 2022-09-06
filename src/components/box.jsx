f60b40459f20354fdd6fc2b9db979a711e6c8294
downred <redonqerimi@gmail.com>
2022-09-06 21:24:07 +0200

    Changed background color and added box component

---
+++ src/components/box.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../index.css";
+
+class Box extends Component {
+  state = {};
+  render() {
+    return <div className="box"></div>;
+  }
+}
+
+export default Box;
